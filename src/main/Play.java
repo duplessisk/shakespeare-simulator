37ad1242f9ad57f1678172fff621fb9ef7c0def9
duplessisk <klduplessis@gmail.com>
2020-11-27 06:09:19 -0800

    added initSentence methods/tests

--- src/main/Play.java
+++ src/main/Play.java
@@ -1,8 +1,9 @@
 import java.util.*;
+import java.lang.*;
 
 public class Play {
 
-    private Map<String, Map<String, Integer>> wordDict;
+    private Map<String, Map<String, Double>> wordMap;
 
     public Play() {}
 
@@ -13,30 +14,70 @@ public class Play {
         }
         return wordList;
     }
-
-    public Map<String, Map<String, Integer>> initWordDict(List<String> wordList) {
-        wordDict = new HashMap();
+    
+    public Map<String, Map<String, Double>> initWordMap(List<String> wordList) {
+        wordMap = new TreeMap();
         for (int i = 0; i < wordList.size() - 1; i++) {
             String word = wordList.get(i), nextWord = wordList.get(i + 1);
-            if (wordDict.keySet().contains(word)) {
-                Map<String, Integer> subDict = wordDict.get(word);
-                if (subDict.keySet().contains(nextWord)) {
-                    subDict.put(nextWord, subDict.get(nextWord) + 1);
+            if (wordMap.keySet().contains(word)) {
+                Map<String, Double> innerMap = wordMap.get(word);
+                if (innerMap.keySet().contains(nextWord)) {
+                    innerMap.put(nextWord, innerMap.get(nextWord) + 1);
                 } else {
-                    subDict.put(nextWord,1);
+                    innerMap.put(nextWord,1.0);
                 }
             } else {
-                wordDict.put(word,new HashMap());
-                Map<String, Integer> subDict = wordDict.get(word);
-                subDict.put(nextWord,1);
+                wordMap.put(word,new TreeMap());
+                Map<String, Double> innerMap = wordMap.get(word);
+                innerMap.put(nextWord,1.0);
             }
         }
-        return wordDict;
+        return wordMap;
     }
 
-    public double weightedCounts(String word, Map<String,Integer> subDict) {
-
-        return 0;
+    public void setWeightedCounts() {
+        for (String word : wordMap.keySet()) {
+            Map<String,Double> innerMap = wordMap.get(word);
+            weightedCounts(innerMap);
+        }
     }
 
+    public void weightedCounts(Map<String,Double> innerMap) {
+        int totalWordCount = totalCount(innerMap);
+        double previousWeightedCount = 0;
+        for (String innerKey : innerMap.keySet()) {
+            double weightedCount = innerMap.get(innerKey)/totalWordCount + previousWeightedCount;
+            innerMap.put(innerKey,weightedCount);
+            previousWeightedCount = weightedCount;
+         }
+    }
+
+    public int totalCount(Map<String,Double> innerMap) {
+        int totalWordCount = 0;
+        for (String innerKey : innerMap.keySet()) {
+            totalWordCount += innerMap.get(innerKey);
+        }
+        return totalWordCount;
+    }
+
+    public List<String> initSentence() {
+        List<String> sentence = new ArrayList();
+        String nextKey = "The";
+        for (int i = 0; i < 10; i++) {
+            sentence.add(nextWord(nextKey));
+        }
+        return sentence;
+    }
+
+    public String nextWord(String nextKey) {
+        Random rand = new Random();
+        double ran = rand.nextDouble();
+        Map<String,Double> innerMap = wordMap.get(nextKey);
+        for (String innerKey : innerMap.keySet()) {
+            if (ran <= innerMap.get(innerKey)) {
+                return innerKey;
+            }
+        }
+        return "ERROR";
+    }
 }
--- src/main/PlayTest.java
+++ src/main/PlayTest.java
@@ -16,7 +16,6 @@ class PlayTest {
         playFile = new Scanner(new File("lib/corpora/Hamlet.txt"));
         play = new Play();
         wordList = play.initWordList(playFile);
-
     }
 
     @Test
@@ -25,51 +24,79 @@ class PlayTest {
     }
 
     @Test
-    void initWordDict_noKeys_returnFalse() {
-        assertFalse(play.initWordDict(wordList).keySet().isEmpty());
+    void initWordMap_noKeys_returnFalse() {
+        assertFalse(play.initWordMap(wordList).keySet().isEmpty());
     }
 
     @Test
-    void initWordDict_emptyKeySet_returnFalse() {
+    void initWordMap_emptyKeySet_returnFalse() {
         // create sample data set
-        Map<String, Map<String,Integer>> wordDict = play.initWordDict(wordList);
-        assertFalse(wordDict.keySet().isEmpty());
-        for (String key : wordDict.keySet()) {
-            assertFalse(wordDict.get(key).keySet().isEmpty());
+        Map<String, Map<String,Double>> wordMap = play.initWordMap(wordList);
+        assertFalse(wordMap.keySet().isEmpty());
+        for (String key : wordMap.keySet()) {
+            assertFalse(wordMap.get(key).keySet().isEmpty());
         }
     }
 
     @Test
-    void initWordDict_correctNumValues_returnTrue() {
-        Map<String, Map<String,Integer>> wordDict = play.initWordDict(wordList);
-        assertFalse(wordDict.keySet().isEmpty());
+    void initWordMap_correctNumValues_returnTrue() {
+        Map<String, Map<String,Double>> wordMap = play.initWordMap(wordList);
+        assertFalse(wordMap.keySet().isEmpty());
         int totalNumValues = 0;
-        for (String key : wordDict.keySet()) {
-            Map<String,Integer> subDict = wordDict.get(key);
-            for (String subKey : subDict.keySet()) {
-                totalNumValues += subDict.get(subKey);
+        for (String key : wordMap.keySet()) {
+            Map<String,Double> subMap = wordMap.get(key);
+            for (String subKey : subMap.keySet()) {
+                totalNumValues += subMap.get(subKey);
             }
         }
-        System.out.println(wordDict.get("'Tis").toString());
-        System.out.println(wordDict.get("a").toString());
         assertFalse(totalNumValues == 0);
         assertTrue(totalNumValues == wordList.size() - 1);
     }
 
     @Test
-    void nextWordsWeighted_maxSumIsOne_returnTrue() {
-        Map<String, Map<String, Integer>> wordDict = play.initWordDict(wordList);
-        Set<String> keySet = wordDict.keySet();
-        assertFalse(wordDict.keySet().isEmpty());
+    void totalCount_notZero_returnTrue() {
+        Map<String, Map<String, Double>> wordMap = play.initWordMap(wordList);
+        assertFalse(wordMap.keySet().isEmpty());
+        for (String outerKey : wordMap.keySet()) {
+            Map<String,Double> innerMap = wordMap.get(outerKey);
+            assertFalse(play.totalCount(innerMap) == 0);
+        }
+    }
+
+    @Test
+    void nextWordsWeighted_weightsAreCorrect_returnTrue() {
+        Map<String, Map<String, Double>> wordMap = play.initWordMap(wordList);
+        play.setWeightedCounts();
+        Set<String> keySet = wordMap.keySet();
+        assertFalse(wordMap.keySet().isEmpty());
         for (String outerKey : keySet) {
-            Iterator<String> subKeysItr = wordDict.get(outerKey).keySet().iterator();
-            assertTrue(wordDict.get(outerKey).get(subKeysItr.next()) == 0);
+            System.out.println(wordMap.get(outerKey).toString());
+            Iterator<String> subKeysItr = wordMap.get(outerKey).keySet().iterator();
             while (subKeysItr.hasNext()) {
                 String nextKey = subKeysItr.next();
                 if (!subKeysItr.hasNext()) {
-                    assertTrue(wordDict.get(outerKey).get(nextKey) == 1);
+                    assertTrue((wordMap.get(outerKey).get(nextKey) > 0.999) &&
+                            (wordMap.get(outerKey).get(nextKey) < 1.001));
+                } else {
+                    assertTrue(wordMap.get(outerKey).get(nextKey) < 1);
                 }
             }
+        }
+    }
+
+    @Test
+    void initSentence_isEmpty_returnFalse() {
+        for (int i = 0; i < 1000; i++) {
+            List<String> sentence = play.initSentence();
+            assertFalse(sentence.isEmpty());
+        }
+    }
+
+    @Test
+    void initSentence_noVoid_returnTrue() {
+        for (int i = 0; i < 1000; i++) {
+            List<String> sentence = play.initSentence();
+            assertFalse(sentence.contains("ERROR"));
         }
     }
 }
