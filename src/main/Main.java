bfa3a773948f48b7e25ccb65b84a348a032f96a7
duplessisk <klduplessis@gmail.com>
2020-11-30 10:26:21 -0800

    v 1.0 complete

--- src/main/Main.java
+++ src/main/Main.java
@@ -19,32 +19,36 @@ public class Main {
                 System.out.println();
                 System.out.println("Word doesn't exist in corpora");
             } else {
-                initSentences(play, rootWord);
+                String sentence = initSentences(play, rootWord);
+                System.out.println();
+                System.out.println(sentence);
             }
         }
     }
-    
+
     public static String getUserInput (Scanner userInput) {
         System.out.println();
         System.out.print("Here is a possible list of root words: ");
-        System.out.println("Hamlet, thy, devil, hitherto, behooves");
+        System.out.println("Hamlet, thy, devil, hitherto, behooves, thou, O,");
         System.out.print("Please select a root word (q to quit): ");
         return userInput.nextLine();
     }
 
-    public static void initSentences(Play play, String rootWord) {
-        while (true) {
-            String sentence = play.initSentence(rootWord);
-            StringTokenizer sentenceTokens = new StringTokenizer(sentence);
-            int wordCount = 0;
-            while (sentenceTokens.hasMoreTokens()) {
-                sentenceTokens.nextToken();
-                wordCount += 1;
-            }
-            if (wordCount >= 3 && wordCount <= 10) {
-                System.out.println();
-                System.out.println(sentence);
-                break;
+    public static String initSentences(Play play, String rootWord) {
+        if (rootWord.contains(".") || rootWord.contains("?") || rootWord.contains("!")) {
+            return "Sentence can't be started with special character";
+        } else {
+            while (true) {
+                String sentence = play.initSentence(rootWord);
+                StringTokenizer sentenceTokens = new StringTokenizer(sentence);
+                int wordCount = 0;
+                while (sentenceTokens.hasMoreTokens()) {
+                    sentenceTokens.nextToken();
+                    wordCount += 1;
+                }
+                if (wordCount >= 2 && wordCount <= 10) {
+                    return sentence;
+                }
             }
         }
     }
--- src/tests/MainTest.java
+++ src/tests/MainTest.java
@@ -1,22 +1,43 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.FileNotFoundException;
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
+
+    Play play;
+    ProcessFile pf;
+    List<String> wordList;
+
+
     @BeforeEach
-    void init() {
-        Play play = new Play();
+    void init() throws FileNotFoundException {
+        play = new Play();
+        pf = new ProcessFile();
+        wordList = pf.initWordList("lib/corpora/Hamlet.txt");
+        play.initWordMap(wordList);
+        play.setWeightedCounts();
     }
 
     @Test
-    void initSentences_infinteLoop_returnFalse() {
-        assertTimeout(Duration.ofMillis(3000), () -> {
-
+    void initSentences_noInfiniteLoop_returnTrue() {
+        assertTimeoutPreemptively(Duration.ofMillis(3000), () -> {
+            Main.initSentences(play,"Hamlet");
         });
     }
+
+    @Test
+    void initSentences_infinteLoop_returnTrue() {
+        for (int i = 0; i < wordList.size() - 1; i++) {
+            String word = wordList.get(i);
+            assertTimeoutPreemptively(Duration.ofMillis(3000), () -> {
+                Main.initSentences(play,word);
+            });
+        }
+    }
+
 }
